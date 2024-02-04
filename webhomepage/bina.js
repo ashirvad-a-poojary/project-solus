b66225ab3e69306ca7af48eb0457ff9aa8358c47
shetty-ishan-9 <ishanshetty.10@gmail.com>
2024-02-04 18:48:40 +0530

    binaural beats page updated

--- webhomepage/bina.js
+++ webhomepage/bina.js
@@ -11,15 +11,13 @@ let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
     {songName: "A BINAURAL JOURNEY", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
+    {songName: "Autumn Sky Meditation", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "Happy Mysterious Piano Relaxation", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Just Relax", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Mind Relaxation", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Morning Garden Acoustic Chill", filePath: "songs/6.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Peaceful Garden Healing", filePath: "songs/7.mp3", coverPath: "covers/7.jpg"},
+    
 ]
 
 songItems.forEach((element, i)=>{ 
