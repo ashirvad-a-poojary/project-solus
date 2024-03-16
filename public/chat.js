e90cbcbe77c4761da9e17a323ead831eb7805aa6
GauravDhanraja <gauravdhanraja88@proton.me>
2024-03-16 12:39:43 +0530

    chat javascript comments

--- public/chat.js
+++ public/chat.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
         } catch (error) {
             console.error('Error sending message to Rasa:', error);
             addChatMessage('bot', 'Sorry, something went wrong while processing your request.');
-            scrollChatboxToBottom(); // Scroll to the bottom after adding a message
+            scrollChatboxToBottom(); 
         }
     };
 
@@ -23,10 +23,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const handleRasaResponse = (data) => {
         const responseText = data[0].text || 'Sorry, I didn\'t understand that.';
         addChatMessage('bot', responseText);
-        scrollChatboxToBottom(); // Scroll to the bottom after adding a message
+        scrollChatboxToBottom(); 
     };
 
-    // Function to add a new chat message to the chat box
     const addChatMessage = (sender, message) => {
         const newMessage = document.createElement('li');
         newMessage.className = `chat ${sender === 'user' ? 'outgoing' : 'incoming'}`;
@@ -34,12 +33,10 @@ document.addEventListener("DOMContentLoaded", function () {
         chatbox.appendChild(newMessage);
     };
 
-    // Function to scroll the chatbox to the bottom
     const scrollChatboxToBottom = () => {
         chatbox.scrollTop = chatbox.scrollHeight;
     };
 
-    // Function to handle sending message
     const sendMessage = () => {
         const userMessage = inputField.value.trim();
         if (userMessage !== '') {
@@ -49,14 +46,12 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     };
 
-    // Event listener for Enter key in the textarea
     inputField.addEventListener('keydown', (event) => {
         if (event.key === 'Enter' && !event.shiftKey) {
-            event.preventDefault(); // Prevents the newline character
+            event.preventDefault(); 
             sendMessage();
         }
     });
 
-    // Event listener for send button click
     sendButton.addEventListener('click', sendMessage);
 });
